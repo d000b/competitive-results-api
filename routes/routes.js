b3e5b024038d2a7c29f2eab1dda6512f18d035e9
d000b <makc.Hitman@gmail.com>
2023-01-16 17:41:53 +0800

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -2,13 +2,37 @@ module.exports = app => {
     const router = require("express").Router();
     const controller = require("../controllers/controller");
     
-    router.get("/", (req, res) => {        
-        res.send({
-            message: "There is one route for each game:",
-            CSGO: "/csgo",
-            Valorant: "/vava",
-            Overwatch: "/overW"
-        });
+    const json_message = {
+        message: "There is one route for each game:",
+        CSGO: "/csgo",
+        Valorant: "/vava",
+        Overwatch: "/overW"
+    };
+
+    router.get("/", (request, results) => {
+        rest_format = request.query.fmt;
+        
+        if ("json" == rest_format)
+        {
+            results.send(json_message);
+        }
+        else if (true || "html" == rest_format)
+        {
+            const filesystem = require('fs');
+
+            filesystem.readFile('routes/index.html', 'utf8', (file_error, file_data) => {
+                if (file_error)
+                {
+                    console.error(file_error);
+                    
+                    results.send(json_message);
+                }
+                else
+                {
+                    results.send(file_data);
+                }
+            });
+        }
     })
 
     router.get("/csgo", controller.csgoResults);
