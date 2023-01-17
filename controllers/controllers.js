796aea927e8c34a9a7e49ee8843b512c3eb0b5c8
d000b <makc.Hitman@gmail.com>
2023-01-18 02:14:05 +0800

    Update controllers.js
     - renamed exported modules

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,14 +1,16 @@
 
-const csgo_tournament_results = require("./csgo");
-const valorant_tournament_results = require("./valorant");
-const overwatch_tournament_results = require("./overwatch");
+const csgo_tournament_results = require('./csgo').csgo;
+const valorant_tournament_results = require('./valorant').valorant;
+const overwatch_tournament_results = require('./overwatch').overwatch;
 
 
-export function get_controller_to_router(router)
+function get_controller_to_router(router)
 {
-    router.get("/csgo", controller.csgo_tournament_results);
+    router.get("/csgo", csgo_tournament_results);
 
-    router.get("/valorant", controller.valorant_tournament_results);
+    router.get("/valorant", valorant_tournament_results);
 
-    router.get("/overwatch", controller.overwatch_tournament_results);
+    router.get("/overwatch", overwatch_tournament_results);
 }
+
+module.exports.routes = get_controller_to_router;
