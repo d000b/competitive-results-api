44e6f9298e0e86ef969a5f59f6f889ffd82144a4
d000b <makc.Hitman@gmail.com>
2023-01-18 02:14:56 +0800

    Update context.js
     - fix parse & wrote data to response
     - export parser class

--- controllers/context.js
+++ controllers/context.js
@@ -1,34 +1,32 @@
+
 const axios = require("axios");
-const cheerio = require("cheerio");
 
 
-export class  ControllerContext
+class  ControllerContext
 {
-    constructor(url, log_header, func_element_parser)
+    constructor(url, func_element_parser, log_header)
     {
         this.uri = url;
         this.header = log_header;
         this.parser = func_element_parser;
     }
 
-    get_result()
+    get_result = function(request, results)
     {
-        const axios = require("axios");
-        const cheerio = require("cheerio");
-
         const host_ = 'http://localhost:8081';
         const header_ = 'Access-Control-Allow-Origin';
 
         const data = { };
-        console.log(this.header)
+        console.log(this.header);
         results.setHeader(header_, host_);
         axios
             .get(this.uri)
-            .then(
-               (response) => {
-
-                    this.parser(response, data)
-                });
-        return data;
+            .then((response) => { 
+                this.parser(response, data); 
+                results.json(data);
+            })
+            .catch(err => console.log);
     }
 }
+
+exports.controller = ControllerContext;
