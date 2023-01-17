8f750f588921501cbd29cab66aae63370deefd43
d000b <makc.Hitman@gmail.com>
2023-01-18 00:35:15 +0800

    Update routes.js
     - Changed the routing of API requests
     - The json response is separated into a new file in the root directory
     - Routing rules sends file controllers

--- routes/routes.js
+++ routes/routes.js
@@ -1,38 +1,24 @@
+
 module.exports = app => {
     const router = require("express").Router();
-    const controller = require("../controllers/controller");
+    const filesystem = require('fs');
+    const controller = require("../controllers/controllers");
+    const sender = require("./sender");
     
     router.get("/", (request, results) => {
         rest_format = request.query.fmt;
         
         if ("json" == rest_format)
         {
-            results.send(json_message);
+            sender.send_data_from_file(filesystem, 'root/index.json', results);
         }
-        else if (true || "html" == rest_format)
+        else if (0 == rest_format.size() || "html" == rest_format)
         {
-            const filesystem = require('fs');
-
-            filesystem.readFile('routes/index.html', 'utf8', (file_error, file_data) => {
-                if (file_error)
-                {
-                    console.error(file_error);
-                    
-                    results.send(json_message);
-                }
-                else
-                {
-                    results.send(file_data);
-                }
-            });
+            sender.send_data_from_file(filesystem, 'root/index.html', results);
         }
     })
 
-    router.get("/csgo", controller.csgoResults);
-
-    router.get("/vava", controller.vavaResults);
-
-    router.get("/overw", controller.overwResults);
+    controller.get_controller_to_router(router);
 
     app.use("/", router);
 }
