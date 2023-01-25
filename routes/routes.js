6d7d9e955919b503889b719f4517549e6cea76b9
d000b <makc.Hitman@gmail.com>
2023-01-26 02:42:05 +0800

    Update routes.js
    - Putting the homepage into the controller module.
    - Scoping of all controllers and their routing outside

--- routes/routes.js
+++ routes/routes.js
@@ -1,23 +1,8 @@
 
 module.exports = app => {
     const router = require("express").Router();
-    const filesystem = require('fs');
     const controller = require("../controllers/controllers");
-    const sender = require("./sender");
     
-    router.get("/", (request, results) => {
-        rest_format = request.query.fmt;
-        
-        if ("json" == rest_format)
-        {
-            sender.send_data_from_file(filesystem, 'root/index.json', results);
-        }
-        else if (true || "html" == rest_format)
-        {
-            sender.send_data_from_file(filesystem, 'root/index.html', results);
-        }
-    })
-
     controller.routes(router);
 
     app.use("/", router);
