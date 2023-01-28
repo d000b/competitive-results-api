f8aaa773b1fc13ba57000cba795bdeefc1b0ae64
d000b <makc.Hitman@gmail.com>
2023-01-28 20:20:08 +0800

    model viewer moved

---
+++ modelview/html.js
@@ -0,0 +1,7 @@
+
+var simple_html_modelview = function(results, data)
+{
+    
+}
+
+module.exports.modelview = simple_html_modelview;
---
+++ modelview/json.js
@@ -0,0 +1,7 @@
+
+var simple_json_modelview = function(results, data)
+{
+    results.json(data);
+}
+
+module.exports.modelview = simple_json_modelview;
