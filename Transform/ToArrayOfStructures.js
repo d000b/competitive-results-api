ece19b180999c3b60d6d6ce28eb381c8a99b4eb9
d000b <makc.Hitman@gmail.com>
2023-02-01 22:13:30 +0800

    Create Transform
     - create transform for 'new' operations
     - (for csv)

---
+++ Transform/ToArrayOfStructures.js
@@ -0,0 +1,8 @@
+// a structure of arrays into an array of structures
+
+var TransformStructureOfArraysIntoArrayOfStructures = function(data)
+{
+
+}
+
+module.exports.ToArrayOfStructures = TransformStructureOfArraysIntoArrayOfStructures;
---
+++ Transform/ToStructureOfArrays.js
@@ -0,0 +1,8 @@
+// an array of structures into a structure of arrays
+
+var TransformArrayOfStructuresIntoStructureOfArrays = function(data)
+{
+
+}
+
+module.exports.ToStructureOfArrays = TransformArrayOfStructuresIntoStructureOfArrays;
